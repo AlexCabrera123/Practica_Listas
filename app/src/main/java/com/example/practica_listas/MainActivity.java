7b31a5a9cb9182f1ab1d6b0accfd2dd1c158e7d8
AlexCabrera123 <100866047+AlexCabrera123@users.noreply.github.com>
2022-10-06 10:03:33 -0500

    06/10/2022 beta

--- app/src/main/java/com/example/practica_listas/MainActivity.java
+++ app/src/main/java/com/example/practica_listas/MainActivity.java
@@ -22,25 +22,22 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private Button bMostrar;
     private EditText etAgregar;
-    private Button bGuardar;
+    private Button bGuardar, bBuscar;
     private ListView lvlista;
     private ArrayList alnumeros;
     private NodoLista primero = null;
-    private TextView promediosueldos, cantidadsueldos, sumasueldos;
     private Double suma,promedio;
-    private int cantidad;
+    private int cantidad,cont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListaEnlazada le = new ListaEnlazada();
-        le.crearListaEnlazada();
         etAgregar = findViewById(R.id.etAgregar);
         bGuardar = findViewById(R.id.bGuardar);
-        sumasueldos = findViewById(R.id.SumaSueldos);
-        cantidadsueldos = findViewById(R.id.CantidadSueldos);
-        promediosueldos = findViewById(R.id.Promedio);
+        bMostrar = findViewById(R.id.bBuscar);
+        lvlista = findViewById(R.id.lvlista);
+
         ArrayList<Double> alnumeros = new ArrayList<>();
         ArrayAdapter<Double> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,alnumeros);
 
@@ -75,6 +72,32 @@ public class MainActivity extends AppCompatActivity {
 
                 lvlista.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
+
+            }
+        });
+
+        bBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input2 = etAgregar.getText().toString().trim();
+                if(input2.equals(""))
+                    Toast.makeText(getApplicationContext(),"FAVOR DE AGREGAR EL NUMERO A BUSCAR",Toast.LENGTH_LONG).show();
+                else {
+                    double aux2 = Double.parseDouble(input2);
+                    while(primero.getDato() == aux2){
+                        cont++;
+                        if (primero.getDato() == aux2){
+                            Log.i("Log","El número está en la posición "+cont);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"NO SE ENCONTRÓ EL NÚMERO",Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    }
+
+                }
+
+
 
             }
         });
